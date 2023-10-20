ea9fb820b3027b99b1b8df1510e21c9f82f5815b
ll novbe <ll novbe@DESKTOP-AKQ170U>
2023-10-20 17:57:51 +0400

    Add project files.

---
+++ excp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+namespace excp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User person1 = new User();
+            Type userType = typeof(User);
+
+
+            PropertyInfo propName = userType.GetProperty("Name", BindingFlags.NonPublic | BindingFlags.Instance);
+            propName.SetValue(person1, "Ferid");
+            Console.WriteLine(propName.GetValue(person1));
+
+            PropertyInfo propSurname = userType.GetProperty("Surname", BindingFlags.NonPublic | BindingFlags.Instance);
+            propSurname.SetValue(person1, "Elizade");
+            Console.WriteLine(propSurname.GetValue(person1));
+
+            PropertyInfo propAge = userType.GetProperty("Age", BindingFlags.NonPublic | BindingFlags.Static);
+            propAge.SetValue(person1, (byte)20);
+            Console.WriteLine(propAge.GetValue(person1));
+
+
+            MethodInfo methodRef = userType.GetMethod("GetFullName", BindingFlags.NonPublic| BindingFlags.Instance);
+            methodRef.Invoke(person1, null);
+
+            MethodInfo changeRef = userType.GetMethod("ChangeAge", BindingFlags.NonPublic | BindingFlags.Static);
+            object[] array = { (byte)21 };
+            changeRef.Invoke(person1, array);
+
+        }
+    }
+}
---
+++ excp/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excp
+{
+    internal class User
+    {
+        string Id { get; set; }
+        string Name { get; set; }
+        string Surname { get; set; }
+        static byte Age { get; set; }
+
+
+        void GetFullName()
+        {
+
+            Console.WriteLine($"Full name is {Name} {Surname}");
+        }
+
+        static void ChangeAge(byte age)
+        {
+            Age = age;
+            Console.WriteLine(Age);
+
+        }
+    }
+}
